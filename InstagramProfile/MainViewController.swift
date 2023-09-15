714dd81982b4c0f0404f04a0b7306f6c70eec82c
Kyle <kyle.pxrk@gmail.com>
2023-09-15 09:24:39 +0900

    Feat: 스토리보드삭제, 초기 루트뷰 지정

---
+++ InstagramProfile/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  InstagramProfile
+//
+//  Created by Kyle on 2023/09/15.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
